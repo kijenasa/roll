209324f27039425a046465a75d7501cae134cc71
Julian <j.sp@tuta.io>
2024-11-08 23:40:33 -0500

    switch srand to miliseconds with sys/time.h

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-#define ASCII "        _-_.\n     _-',^. `-_.\n ._-' ,'   `.   `-_\n!`-_._________`-':::\n!   /\\        /\::::\n;  /  \\  %02d  /..\:::\n! /    \\    /....\\::\n!/      \\  /......\\:\n;--.___. \\/_.__.--;;\n '-_    `:!;;;;;;;'\n    `-_, :!;;;''\n        `-!'\n"
+#define d20art "        _-_.\n     _-',^. `-_.\n ._-' ,'   `.   `-_\n!`-_._________`-':::\n!   /\\        /\::::\n;  /  \\  %02d  /..\:::\n! /    \\    /....\\::\n!/      \\  /......\\:\n;--.___. \\/_.__.--;;\n '-_    `:!;;;;;;;'\n    `-_, :!;;;''\n        `-!'\n"
 
 int roll(int size) {
-	int num = 1 + rand() % (size - 1);
+	int num = (rand() % size) + 1;
 	return num;
 }
 
-int main() {
-	srand(clock());
+int main(int argc, char* argv[]) {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	long milliseconds = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	srand(milliseconds);
+
 	int value = roll(20);
-	printf(ASCII, value);
+	printf(d20art, value);
 }
